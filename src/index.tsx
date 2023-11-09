2f04c2f9e7fb98ba0fb9c48b105515f84f1cdaf5
Simi Hartstein <simihartstein@users.noreply.github.com>
2023-11-09 09:18:29 -0500

    Useless change

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
+// @Copyright Simi Hartstein
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
